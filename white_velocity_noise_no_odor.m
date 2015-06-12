37479f9ef6dcba6366cd27912dbc47e86b6e9bfe
Rich Pang <rpang.contact@gmail.com>
2015-06-12 13:17:39 -0700

    Make a few minor edits.

--- white_velocity_noise_no_odor.m
+++ white_velocity_noise_no_odor.m
@@ -1,12 +1,12 @@
 % run PControl and set things up before running this script
 
 %%
-INSECT = '';
-Y = 0; % which stripe set to use (1 - 2 stripes, 2 - 4 stripes, 3 - 6 stripes, 4 - 8 stripes, 5 - 12 stripes)
+INSECT = '15_tr3';
+Y = 4; % which stripe set to use (1 - 2 stripes, 2 - 4 stripes, 3 - 6 stripes, 4 - 8 stripes, 5 - 12 stripes)
 
-DATE = datestr(now, 'YYMMDD');
-DURATION = 10 * 60; % in seconds
-STARTX = 1; % starting x position
+DATE = datestr(now, 'YYmmDD');
+DURATION = 7 * 60; % in seconds
+STARTX = 91; % starting x position
 EDR_DIR = 'C:\Users\researcher\Documents\Rich\edr_files_visual_expt';
 
 % create com server connected to WinEDR
@@ -17,20 +17,25 @@ end
 %%
 Panel_com('stop');
 Panel_com('set_position', [STARTX, Y]);
-pause(1);
-Panel_com('start');
+
 
 %% begin recording
 recording_start = datestr(now,'HHMMSS');
+disp('Recording started.');
 fname = [DATE '_' recording_start '_stripes_insect' INSECT '_ypos_' num2str(Y)];
 edr_path = [EDR_DIR '\' fname];
 
+
 WinEDR.NewFile(edr_path);
 WinEDR.StartRecording;
+pause(5);
+Panel_com('start');
 
 tic;
-while toc < tic + DURATION
+while toc < DURATION
     
 end
 
 WinEDR.StopRecording;
+
+disp('Recording over.');

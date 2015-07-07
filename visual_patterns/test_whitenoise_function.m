6f87b92593970efed22b031425b987d5b5413234
Rich Pang <rpang.contact@gmail.com>
2015-07-07 16:26:06 -0700

    Add some comments.

--- visual_patterns/test_whitenoise_function.m
+++ visual_patterns/test_whitenoise_function.m
@@ -1,15 +1,22 @@
+% in this script we'll create the position function corresponding to white velocity noise of several difference standard deviations
+% set up parameters
 FS = 100; % Hz
 T = 20; % minutes
 NUM_TIMESTEPS = FS * T * 60; % T minutes at FS Hz
-VEL_STDS = 0.25:0.25:4;
+VEL_STDS = 0.25:0.25:4; % standard deviations
 
+% loop over all standard deviations
 for ctr = 1:length(VEL_STDS)
+    % make velocity function
     vel_std = VEL_STDS(ctr);
     func = vel_std * randn(1, NUM_TIMESTEPS);
+    % save velocity function
     fname = ['function_white_noise_velocity_std_' num2str(vel_std) '.mat'];
     save(fname, 'func');
-    
+   
+    % make position function (this is done by summing velocity function, converting positions to integers, and modding it so that positions > 96 wrap around 
     func = mod(round(cumsum(func)), 96) + 1;
+    % save position function
     fname = ['position_function_white_noise_velocity_std_' num2str(vel_std) '.mat'];
     save(fname, 'func');
 end
